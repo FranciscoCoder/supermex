063bfd60089ba20a99d60296d73435478093270a
Francisco Manuel Marquez Marin <fcommm@gmail.com>
2022-06-23 20:32:20 +0200

    primer fetch de idioma en inicio

--- backend/src/Controller/CategoriasRecetasController.php
+++ backend/src/Controller/CategoriasRecetasController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api/categories-recipes")
+ */
 class CategoriasRecetasController extends AbstractController
 {
     /**
-     * @Route("/categorias/recetas", name="app_categorias_recetas")
+     * @Route("/", name="app_categorias_recetas_index", methods={"GET"})
      */
     public function index(): Response
     {
