14e55b86a16591c853647094a51c18a150c0e5b6
Francisco <fcommm@gmail.com>
2022-07-05 00:34:04 +0200

    creado controlador usuario, arreglar problemas en endpoints

---
+++ backend/src/Controller/UsuarioController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsuarioController extends AbstractController
+{
+    /**
+     * @Route("/api/users", name="app_users_index", methods={"GET"})
+     */
+    public function index(UsuarioRepository $usuarioRepository, Request $request): Response
+    {
+        $page = $request->query->get('page', 1);
+        $limit = $request->query->get('limit', 20);
+
+        if($page<=0){$page=1;}
+
+        //calculamos el offset de los registros
+        $offset = ($page-1)*$limit;
+
+        $usuarios = $usuarioRepository->findBy([], ['id' => 'DESC'], $limit, $offset);
+        $totalRegistros = count($usuarios);
+        $resultado = [];
+
+        foreach ($usuarios as $usuario){
+            $resultado[]=[
+                'id' => $usuario->getId(),
+                'nombre' => $usuario->getNombre(),
+                'username' => $usuario->getUserIdentifier(),
+                'rol' => $usuario->getRoles(),
+                'fecha_creacion' => $usuario->getFechaCreacion()->format('d-m-Y H:i'),
+            ];
+        }
+        
+        return $this->json([
+            "result" => $resultado ,
+            "count" => $totalRegistros
+        ]);
+    }
+
+    /**
+     * @Route("/api/users/{username}", name="app_users_show", methods={"GET"})
+     */
+    public function showRecipe(UsuarioRepository $usuarioRepository, $username): Response
+    {
+        $usuario = $usuarioRepository->findOneBy(['username'=>$username]);
+        if($usuario === null){
+            throw $this->createNotFoundException();
+        }
+
+        $resultado[]=[
+            'id' => $usuario->getId(),
+            'nombre' => $usuario->getNombre(),
+            'username' => $usuario->getUserIdentifier(),
+            'rol' => $usuario->getRoles(),
+            'fecha_creacion' => $usuario->getFechaCreacion()->format('d-m-Y H:i')
+        ];
+
+        return $this->json([
+            'result' => $resultado,
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/", name="app_user_register", methods={"POST"})
+     */
+    public function newRecipe(Request $request, EntityManagerInterface $em): Response
+    {
+        $nombreRequest = $request->request->get("nombre");
+        $usuarioRequest = $request->request->get("usuario");
+        $rolRequest = $request->request->get("rol");
+        $passwordRequest = $request->request->get("password");
+
+        //$data = $request->toArray();
+        $resultado="ko";
+        $registro='';
+
+        if((!empty($usuarioRequest))&&(!empty($passwordRequest))){
+            $usuario = new Usuario();
+            $usuario->setNombre($nombreRequest);
+            $usuario->setUsername($usuarioRequest);
+            $usuario->setRoles([$rolRequest]);
+            $usuario->setPassword($passwordRequest);
+            $usuario->setFechaCreacion(new \DateTime());
+
+            $em->persist($usuario);
+            $em->flush();
+
+            $registro='';
+            if(!empty($usuario->getId()))
+            {
+                $resultado="ok";
+                $registro=$usuario->getId();
+            }
+        }
+
+        return $this->json([
+            'result' => $resultado,
+            'id' => $registro
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/{id}", name="app_recipes_update", methods={"PUT"})
+     */
+    public function updateRecipe(Request $request, UsuarioRepository $usuarioRepository, EntityManagerInterface $em, $id): Response
+    {
+        $nombreRequest = $request->request->get("nombre");
+        $usuarioRequest = $request->request->get("usuario");
+        $rolRequest = $request->request->get("rol");
+        $passwordRequest = $request->request->get("password");
+
+        //Comprobamos si existe el registro
+        $usuario = $usuarioRepository->find($id);
+        $resultado="ko";
+
+        if(!empty($usuario->getId()))
+        {
+            $usuario->setNombre($nombreRequest);
+            $usuario->setUsername($usuarioRequest);
+            $usuario->setRoles([$rolRequest]);
+            $usuario->setPassword($passwordRequest);
+            $em->flush();
+
+            $resultado="ok";
+        }
+        
+        return $this->json([
+            'result' => $resultado,
+            'id' => $usuario->getId(),
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/{id}", name="app_recipes_detele", methods={"DELETE"})
+     */
+    public function deleteRecipe(UsuarioRepository $usuarioRepository, EntityManagerInterface $em, $id): Response
+    {
+        $resultado="ko";
+        if(!empty($id)){
+            $receta = $usuarioRepository->find($id);
+            $em->remove($receta);
+            $em->flush();
+            $resultado="ok";
+        }
+        
+        return $this->json([
+            'result' => $resultado,
+        ]);
+    }
+}
--- backend/src/Entity/Usuario.php
+++ backend/src/Entity/Usuario.php
@@ -35,6 +35,16 @@ class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $fecha_creacion;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -117,5 +127,29 @@ class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getFechaCreacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_creacion;
+    }
+
+    public function setFechaCreacion(\DateTimeInterface $fecha_creacion): self
+    {
+        $this->fecha_creacion = $fecha_creacion;
+
+        return $this;
     }
 }
